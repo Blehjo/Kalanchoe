1fb48c09a0ebfc6cfd8e0fadb70cd42af36f0f56
bseton11-YJKnqQ <bseton11@gmail.com>
2022-12-29 05:37:40 -0600

    added routes

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,20 +1,76 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import House from "./components/House";
+import Authentication from "./components/Authentication";
+import Chats from "./components/Chats";
+import Communities from "./components/Communities";
+import Community from "./components/Community";
+import Dashboard from "./components/Dashboard";
+import Dilemmas from "./components/Dilemmas";
+import Explore from "./components/Explore";
+import LandingPage from "./components/LandingPage";
+import Message from "./components/Message";
+import Messages from "./components/Messages";
+import Posts from "./components/Posts";
+import Profile from "./components/Profile";
+import SinglePost from "./components/SinglePost";
+import Users from "./components/Users";
+
 
 const AppRoutes = [
   {
     index: true,
-    element: <House />
+    element: <Dashboard />
   },
   {
-    path: '/counter',
-    element: <Counter />
+    path: '/authentication',
+    element: <Authentication />
   },
   {
-    path: '/fetch-data',
-    element: <FetchData />
+    path: '/chats',
+    element: <Chats />
   },
+  {
+    path: '/communites',
+    element: <Communities />
+  },
+  {
+    path: '/community/:id',
+    element: <Community />
+  },
+  {
+    path: '/projects',
+    element: <Dilemmas />
+  },
+  {
+    path: '/explore',
+    element: <Explore />
+  },
+  {
+    path: '/landingpage',
+    element: <LandingPage />
+  },
+  {
+    path: '/message/:id',
+    element: <Message />
+  },
+  {
+    path: '/messages',
+    element: <Messages />
+  },
+  {
+    path: '/posts',
+    element: <Posts />
+  },
+  {
+    path: '/profile',
+    element: <Profile />
+  },
+  {
+    path: '/posts/:id',
+    element: <SinglePost />
+  },
+  {
+    path: '/users',
+    element: <Users />
+  }
 ];
 
 export default AppRoutes;
