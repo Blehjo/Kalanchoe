f44ba254a786800aeb4f73a131e19a130cc222d0
Blehjo <blehjooo@gmail.com>
2022-12-30 10:41:21 -0600

    added panels component and draggable interface

--- ClientApp/src/components/Profile.js
+++ ClientApp/src/components/Profile.js
@@ -1,11 +1,150 @@
-﻿import { Fragment } from "react";
+﻿import React, { Fragment } from "react";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { Col } from 'react-bootstrap';
 
 const Profile = () => {
+     // using useCallback is optional
+    const onBeforeCapture = useCallback(() => {
+        /*...*/
+    }, []);
+    const onBeforeDragStart = useCallback(() => {
+        /*...*/
+    }, []);
+    const onDragStart = useCallback(() => {
+        /*...*/
+    }, []);
+    const onDragUpdate = useCallback(() => {
+        /*...*/
+    }, []);
+    const onDragEnd = useCallback(() => {
+        // the only one that is required
+    }, []);
+
     return(
         <Fragment>
             <h1>Studio</h1>
-        </Fragment >
+            <DragDropContext
+                onBeforeCapture={onBeforeCapture}
+                onBeforeDragStart={onBeforeDragStart}
+                onDragStart={onDragStart}
+                onDragUpdate={onDragUpdate}
+                onDragEnd={onDragEnd}
+            >
+                
+            </DragDropContext>
+        </Fragment>
     );
 }
 
-export default Profile;
+{/* export default Profile;
+
+// fake data generator
+const getItems = (count) => Array.from({length: count}, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+}));
+  
+  // a little function to help us with reordering the result
+const reorder =  (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+  
+const grid = 8;
+  
+const getItemStyle = (draggableStyle, isDragging) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: 'none',
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? 'lightgreen' : 'grey',
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+  
+const getListStyle = (isDraggingOver) => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    width: 350
+});
+        
+export class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: getItems(10)
+        }
+        this.onDragEnd = this.onDragEnd.bind(this);
+    }
+    
+    onDragEnd (result) {
+        // dropped outside the list
+        if(!result.destination) {
+            return; 
+        }
+        
+        const items = reorder(
+            this.state.items, 
+            result.source.index, 
+            result.destination.index
+        );
+        
+        this.setState({
+            items
+        });
+    }
+    
+    // Normally you would want to split things out into separate components.
+    // But in this example everything is just done in one place for simplicity
+    render() {
+       return (
+        <Fragment>
+        <h1>Studio</h1>
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                <Droppable droppableId="droppable">
+                    {(provided, snapshot) => (
+                    <div 
+                        ref={provided.innerRef} 
+                        style={getListStyle(snapshot.isDraggingOver)}
+                        {...provided.droppableProps}
+                    >
+                        {this.state.items.map((item, index) => (
+                        <Draggable
+                            key={item.id}
+                            draggableId={item.id}
+                            index={index}
+                        >
+                            {(provided, snapshot) => (
+                            <div>
+                                <div
+                                ref={provided.innerRef}
+                                {...provided.dragHandleProps}
+                                {...provided.draggableProps}
+                                style={getItemStyle(
+                                    provided.draggableProps.style,
+                                    snapshot.isDragging
+                                    )}
+                                >
+                                <textarea/>
+                                {item.content}
+                                </div>
+                                {provided.placeholder}
+                            </div>
+                            )}
+                        </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        </Fragment>
+       ); 
+    }
+} */}
