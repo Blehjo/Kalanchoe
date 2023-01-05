d70127284918e9659684df7390c751d743feeae3
bseton11-YJKnqQ <bseton11@gmail.com>
2023-01-04 18:19:56 -0600

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using KalanchoeAI.Models;
-
+using OpenAI.GPT3.Extensions;
+using OpenAI.GPT3.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-    
+//var openAiApiKey = builder.Configuration["OpenAI:ApiKey"];
+
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
@@ -17,21 +19,25 @@ builder.Services.AddDbContext<KalanchoeAIDatabaseContext>(options =>
 
 var app = builder.Build();
 
+var newBuilder = new ConfigurationBuilder()
+    .AddUserSecrets<Program>();
+
+IConfiguration configuration = newBuilder.Build();
+var openAiApiKey = configuration["OpenAI:ApiKey"];
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddScoped(_ => configuration);
+
+serviceCollection.AddOpenAIService(openAiApiKey);
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var sdk = serviceProvider.GetRequiredService<IOpenAIService>();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-var newbuilder = new ConfigurationBuilder()
-    .AddJsonFile("ApiSettings.json")
-    .AddUserSecrets<Program>();
-
-IConfiguration configuration = newbuilder.Build();
-var serviceCollection = new ServiceCollection();
-serviceCollection.AddScoped(_ => configuration);
-//serviceCollection.AddOpenAIService();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
