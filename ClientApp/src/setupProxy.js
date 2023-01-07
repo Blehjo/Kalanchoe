c97d3f8cccf6439b3b77341b6861977ef3844676
bseton11-YJKnqQ <bseton11@gmail.com>
2023-01-07 09:35:34 -0600

    connected to backedn

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -1,8 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:23474';
+const target = 'https://localhost:7028';
 
 const context = [
   "/weatherforecast",
